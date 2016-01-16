7536815bb88a8903dd75d18b1abcab6b57875170
RSGC-dubczak-m <student@MacBook-Pro-0446-MDubczak.local>
2016-01-15 21:04:51 -0500

    First Working Commit
    The program works as it should, however is still crude and without any
    comments, etc.

--- command-line-example/main.swift
+++ command-line-example/main.swift
@@ -1,79 +1,12 @@
 //
 //  main.swift
-//  basic-command-line
+//  reducing-radicals
 //
-//  Created by Russell Gordon on 2015-12-16.
+//  Created by Maxym Dubczak on 2015-12-16.
 //  Copyright © 2015 Royal St. George's College. All rights reserved.
 //
 
 import Foundation
-/*
-// Print welcome message on it's own line
-print("Welcome.")
-
-
-// Print prompt (but don't go to new line)
-print("What is your name? ", terminator: "")
-
-// Get the user's name
-var givenName = readLine(stripNewline: true)
-
-// Say hello, so long as some input was provided
-// Add an extra newline at the start     \n
-if let name = givenName {
-    print("\nNice to meet you, \(name)!")
-}
-
-
-// Ask about food preferences
-var validInput : Bool          // Status of input
-repeat {
-    
-    // Show prompts
-    print("\n What is your favourite food category?")
-    print("A - Cereal")
-    print("B - Fruit")
-    print("C - Vegetables")
-    print("Select your favourite (A/B/C/Quit): ", terminator: "")
-    
-    // Get the user's input
-    var givenFoodPreference = readLine(stripNewline: true)
-    
-    // Assume valid input
-    validInput = true
-    
-    // Interpret the result, if one was given
-    if let foodPreference = givenFoodPreference {
-        
-        // Add a blank line to separate response from input block
-        print("")
-        
-        switch foodPreference {
-            
-        case "A", "a":
-            print("Awesome, I love Cheerios!")
-            
-        case "B", "b":
-            print("My favourite is mango.")
-            
-        case "C", "c":
-            print("Have you tried baked eggplant? Delectable!")
-            
-        case "Quit", "QUIT", "quit":
-            print("Bye.")
-            
-        default:
-            print("Sorry, I didn't get that.")
-            validInput = false
-        }
-        
-    }
-    
-} while validInput == false
-
-// Print one additional blank line to separate from exit code message that is automatically displayed
-print("")
-*/
 
 var counter = Float(0.00)
 
@@ -81,20 +14,26 @@ print("What radical would you like to reduce?")
 
 print("√", terminator:"")
 
-//var input = NSString(data: NSFileHandle.fileHandleWithStandardInput().availableData, encoding:NSUTF8StringEncoding)
-
 var inputString = readLine(stripNewline: true)
 
 var inputNumber: Float = NSString(string: inputString!).floatValue
 
 var factorCounts = [Int]()
 
+var outsideNumber = [Int]()
+
+var outsideFinal = 1
+
+var insideNumber = [Int]()
+
+var insideFinal = 1
+
 if (inputNumber == 0.0){
     print("Sorry this isn't factorable")
     exit(0)
 }
 
-    print("\nOK reducing √\(inputNumber)!")
+print("\nOK reducing √\(Int(inputNumber))")
 
 
 while (counter <= sqrt(inputNumber)) {
@@ -109,6 +48,56 @@ while (counter <= sqrt(inputNumber)) {
 factorCounts += [Int(inputNumber)]
 print(factorCounts)
 
-//for (index, value) in factorCounts.enumerate() {
-  //  print("\(index) -> \(value)")
-//}
+var factorDictionary: [Int:Int] = [:]
+
+for factors in factorCounts {
+    if factorDictionary[factors] == nil {
+        
+        factorDictionary[factors] = 1 //first time
+
+
+    } else if(factorDictionary[factors] == 0){
+        //already in dictionary
+        //adding one into current count
+
+        factorDictionary[factors]! = factorDictionary[factors]! + 1
+    }else if(factorDictionary[factors] == 1){
+        //already in dictionary
+        //adding one into current count
+        outsideNumber.append(factors)
+
+        factorDictionary[factors]! = factorDictionary[factors]! - 1
+
+    }
+}
+for number in outsideNumber {
+    outsideFinal = outsideFinal * number
+}
+
+for factors in factorCounts {
+    if factorDictionary[factors] == 1 {
+        insideNumber.append(factors)
+    }
+}
+
+for number in insideNumber {
+    insideFinal = insideFinal * number
+}
+
+//print(outsideNumber)
+//print(outsideFinal)
+//print(factorDictionary)
+
+print("Factors down to \(outsideFinal)√\(insideFinal)")
+/*
+for count in factorCounts{
+if (factorDictionary[count]!%2 == 0 && factorDictionary[count] > 0){
+
+factorDictionary[count] = factorDictionary[count]! - 2
+
+//var outsideNumber = [Int](factorDictionary.keys)
+//print(outsideNumber)
+print(factorDictionary[count])
+}
+}
+*/
